3611e7fbff698337339aafbd37d6b45c4b212f62
yudirunat <yudi.berland@gmail.com>
2024-07-05 02:04:48 +0700

    test sql pt seru

--- testSqlPtSeru.sql
+++ testSqlPtSeru.sql
@@ -1,3 +1,5 @@
+-- Bikin table semua
+
 CREATE TABLE teachers (
     id serial,
     name varchar(100),
@@ -5,15 +7,31 @@ CREATE TABLE teachers (
     PRIMARY key (id)
 );
 
-SELECT * from classes;
-
-CREATE TABLE classes ( id serial, 
+CREATE TABLE classes ( 
+id serial, 
 name VARCHAR(50), 
-teacher_id INT, PRIMARY KEY(id), 
-FOREIGN KEY (teacher_id) REFERENCES teachers(id));
+teacher_id INT, 
+PRIMARY KEY(id), 
+FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+);
+
+CREATE TABLE students ( 
+id serial, 
+name VARCHAR(100), age INT,
+class_id INT, PRIMARY KEY(id), 
+FOREIGN KEY (class_id) REFERENCES classes(id) 
+);
+
+-- input data semua
+
+INSERT INTO teachers (name, subject) VALUES ('Pak Anton', 'Matematika');
+INSERT INTO teachers (name, subject) VALUES ('Bu Dina', 'Bahasa Indonesia'); 
+INSERT INTO teachers (name, subject) VALUES ('Pak Eko', 'Biologi');
 
 
-CREATE TABLE students ( id serial, name VARCHAR(100), age INT, class_id INT, PRIMARY KEY(id), FOREIGN KEY (class_id) REFERENCES classes(id) );
+INSERT INTO classes (name, teacher_id) VALUES ('Kelas 10A', 1); 
+INSERT INTO classes (name, teacher_id) VALUES ('Kelas 11B', 2); 
+INSERT INTO classes (name, teacher_id) VALUES ('Kelas 12C', 3);
 
 
 INSERT INTO students (name, age, class_id) VALUES ('Budi', 16, 1); 
@@ -21,51 +39,26 @@ INSERT INTO students (name, age, class_id) VALUES ('Ani', 17, 2);
 INSERT INTO students (name, age, class_id) VALUES ('Candra', 18, 3);
 
 
-INSERT INTO classes (name, teacher_id) VALUES ('Kelas 10A', 1); 
-INSERT INTO classes (name, teacher_id) VALUES ('Kelas 11B', 2); 
-INSERT INTO classes (name, teacher_id) VALUES ('Kelas 12C', 3);
+-- soal nomor 1 
 
-SELECT * from teachers;
-
-SELECT * from students;
-
-INSERT INTO teachers (name, subject) VALUES ('Pak Anton', 'Matematika');
-
-INSERT INTO teachers (name, subject) VALUES ('Bu Dina', 'Bahasa Indonesia'); 
-
-INSERT INTO teachers (name, subject) VALUES ('Pak Eko', 'Biologi');
-
-UPDATE classes set id = 3
-WHERE id = 5;
-
-SELECT * from classes;
-
-DELETE from teachers where name = 'Pak Anton';
-SELECT * from students;
 
 SELECT 
-*
+    students.name AS nama_murid, 
+    classes.name AS nama_kelas, 
+    teachers.name AS nama_guru
 FROM 
-    students
+    students 
 JOIN 
-    classes ON students.class_id = classes.id
-JOIN
+    classes ON students.class_id = classes.id 
+JOIN 
     teachers ON classes.teacher_id = teachers.id;
 
 
-SELECT * from teachers;
-SELECT * from classes;
+-- soal nomor 2 
+
+
 
 SELECT 
-    teachers.name AS teacher_name,
-    classes.name AS kelas_nama
-FROM 
-    teachers
-JOIN 
-    classes ON teachers.id = classes.teacher_id;
-
-
-    SELECT 
     teachers.name AS teacher_name, 
     STRING_AGG(classes.name, ', ') AS class_list 
 FROM 
@@ -76,25 +69,9 @@ GROUP BY
     teachers.name;
 
 
+-- soal nomor 3
 
-    CREATE VIEW student_class_teacher AS 
-SELECT 
-    students.name AS student_name, 
-    classes.name AS class_name, 
-    teachers.name AS teacher_name 
-FROM 
-    students 
-JOIN 
-    classes ON students.class_id = classes.id 
-JOIN 
-    teachers ON classes.teacher_id = teachers.id;
-
-    SELECT * FROM student_class_teachers;
-
-
--- 3 
-
-CREATE VIEW student_class_teachers AS 
+CREATE VIEW murid_kelas_guru AS 
 SELECT 
     students.name AS nama_murid, 
     classes.name AS nama_kelas, 
@@ -106,17 +83,10 @@ JOIN
 JOIN 
     teachers ON classes.teacher_id = teachers.id;
 
+SELECT * from murid_kelas_guru;
 
 
-
-
-
-SELECT * from students
-join classes on students.class_id = classes.id
-JOIN teachers on classes.teacher_id = teachers.id;
-
-
--- 4
+-- soal nomor 4 
 
 CREATE OR REPLACE FUNCTION get_murid_kelas_guru() RETURNS TABLE (
     murid_name VARCHAR,
@@ -139,22 +109,14 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-SELECT * from get_murid_kelas_guru()
+SELECT * from get_murid_kelas_guru();
 
--- 5
+
+-- soal nomor 5
 
 ALTER TABLE students ADD CONSTRAINT unique_student UNIQUE (name, age, class_id);
 ALTER TABLE classes ADD CONSTRAINT unique_class UNIQUE (name, teacher_id);
 ALTER TABLE teachers ADD CONSTRAINT unique_teacher UNIQUE (name, subject);
-
-INSERT into classes (name,teacher_id)
-VALUES ('Kelas 10D', 1);
-
-
-
-SELECT * from students;
-SELECT * from teachers;
-SELECT * FROM classes;
 
 
 
